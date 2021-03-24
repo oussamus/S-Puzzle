2aabadca306e23ab1aac3bc03dc3d4c0b7b3cafc
danon360 <fakhrdany@gmail.com>
2021-03-24 15:40:33 -0400

    made some adjustments

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,4 +1,7 @@
 import copy
+from collections import deque
+open_list = deque()
+closed_list = list()
 
 class Tree(object):
     '''Tree that will contain a state in its data'''
@@ -9,29 +12,113 @@ class Tree(object):
         self.depth = None
 
 
-def depthFirst(Tree):
+def depthFirst():
     '''Not Complete'''
     current_node = Tree
-    while True:
-        child_state = generateChild(current_node.data)
-        current_state = child_state
-        if not current_state:
+    global open_list
+    # generate the child state
+    #child_state = generateChild(current_node.data)
+
+    counter =0
+    while len(open_list) > 0:
+
+        if counter == 4:
+            counter =0
+            print(current_state.log)
+        counter += 1
+        current_state = open_list.pop()
+        closed_list.append(current_state)
+        
+        #print(len(open_list))
+        #if current state is not null (i.e if a child exists
+        if current_state:
+
+            #check if it is the final state
             if current_state.isFinal():
-                print("final")
+                print(current_state)
                 break
+            #if not, creat anew tree node, fit the child state into it, set all the variables for the node, and set current node to that node
             else:
+
+                children = generateAllChildren(current_state)
+                open_list.extend(children)
+                #open_list = children + open_list
+                '''
                 temp_node = Tree()
+                #setting parameters
                 temp_node.depth = current_node.depth +1
                 temp_node.parent = current_node
-                temp_node.data = generateChild(current_state)
+                #inserting the state in the open queue
+                open_list.append(current_state)
+                #fitting the child state
+                temp_node.data = generateChild(open_list.pop())
+
+                #adding the child node to the children of the current node
                 current_node.children.append(temp_node)
 
+                #setting the current node = to the child node
                 current_node = temp_node
-
-
-
+                '''
+        #if there are no child states in the current state
         else:
+            '''
+            #add the current state into the closed list
+            closed_list.append(current_state)
+            current_node = current_node.parent
             print("no")
+            '''
+def difference(child_list, open_list, closed_list):
+    returning_list = list()
+    for state in child_list:
+        if state in open_list or state in closed_list:
+            continue
+        else:
+            returning_list.append(state)
+    return returning_list
+
+
+
+def generateAllChildren(state):
+    returning = list()
+
+    # iterating through all the possible moveUp actions
+    for i in list(range(0, rows)):
+        for j in list(range(0, column)):
+            out = state.moveUp(i, j)
+
+            if not out: continue
+
+
+            returning.append(out)
+
+    # iterating through all the possible moveDown actions
+    for i in list(range(0, rows)):
+        for j in list(range(0, column)):
+            out = state.moveDown(i, j)
+
+            if not out: continue
+
+            returning.append(out)
+    # iterating through all the possible moveRight actions
+    for i in list(range(0, rows)):
+        for j in list(range(0, column)):
+            out = state.moveRight(i, j)
+
+            if not out: continue
+
+
+            returning.append(out)
+    # iterating through all the possible moveLeft actions
+    for i in list(range(0, rows)):
+        for j in list(range(0, column)):
+            out = state.moveLeft(i, j)
+
+            if not out: continue
+
+            returning.append(out)
+
+    filtered_list = difference(returning,open_list,closed_list)
+    return filtered_list
 
 def generateChild(state):
     '''takes a state as a parameter, tries all possible actions, and returns a child state or None when there are no more child states '''
@@ -44,7 +131,7 @@ def generateChild(state):
 
             if not out: continue
 
-            if out in open_list:
+            if out in open_list or out in closed_list:
                 continue
             else:
                 return out
@@ -56,7 +143,7 @@ def generateChild(state):
 
             if not out: continue
 
-            if out in open_list:
+            if out in open_list or out in closed_list:
                 continue
             else:
                 return out
@@ -67,7 +154,7 @@ def generateChild(state):
 
             if not out: continue
 
-            if out in open_list:
+            if out in open_list or out in closed_list:
                 continue
             else:
                 return out
@@ -78,7 +165,7 @@ def generateChild(state):
 
             if not out: continue
 
-            if out in open_list:
+            if out in open_list or out in closed_list:
                 continue
             else:
                 return out
@@ -90,6 +177,9 @@ class State(object):
     def __init__(self, Data, log ):
         self.Data = Data
         self.log = log
+        self.parent = None
+        self.children = list()
+        self.depth = None
 
     def addLog(self, log):
         self.log.append(log)
@@ -108,7 +198,7 @@ class State(object):
             return new_state    #returns the new child state
 
         else:
-            print("No Move\n")
+            return None
 
     def moveRight(self, row, col):
         if (col % column) < (column - 1):
@@ -120,7 +210,7 @@ class State(object):
             new_state.log.append((row, col, "right"))
             return new_state
         else:
-            print("No Move\n")
+            return None
 
     def moveDown(self, row, col):
         if row < rows -1:
@@ -132,7 +222,7 @@ class State(object):
             new_state.log.append((row, col, "down"))
             return new_state
         else:
-            print("No Move\n")
+            return None
 
     def moveUp(self, row, col):
         if row > 0 and row <= rows :
@@ -154,6 +244,7 @@ class State(object):
             return False
 
     def isFinal(self):
+        global goal
         if self.Data == goal:
             return True
         else:
@@ -161,26 +252,26 @@ class State(object):
 column =3
 rows = 3
 goal = [[1,2,3],[4,5,6],[7,8,9]]
-open_list = list()
-closed_list = None
+
 
 if __name__ == '__main__':
 
-    init = [[9, 8, 5], [4, 5, 3], [7, 2, 1]]
+    init = [[1, 2, 3], [4, 5, 6], [8, 7, 9]]
+
+    open_list = deque()
+    closed_list = list()
 
     #starting intial state
     s = State(init,list())
+    s.depth = 1
 
     #creating tree root
-    root = Tree()
+    #root = Tree()
 
     #loading the initial state into the root of the tree
-    root.data = s
+    #root.data = s
 
     #adding the inital state to the open list
     open_list.append(s)
-
-    #test stuff
-    open_list.append(s.moveUp(1,0))
-    generateChild(s)
+    depthFirst()
 
