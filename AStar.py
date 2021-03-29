d66c1a0ff47c9903fc37295c188bc0ebd9aee1c9
oussamus <44511759+oussamus@users.noreply.github.com>
2021-03-28 23:39:12 -0400

    Create AStar.py
    Still working on it. The heuristic function is working properly. 
    I am still working on generate_possible_children function. The generate_child() function is ignored for the moment.

---
+++ AStar.py
@@ -0,0 +1,190 @@
+class Node:
+    def __init__(self,data,level,fval):
+        """ Initialize the node with the data, level of the node and the calculated fvalue """
+        self.data = data
+        self.level = level
+        self.fval = fval
+
+    def generate_child(self):
+        """ Generate child nodes from the given node by moving the blank space
+            either in the four directions {up,down,left,right} """
+        x,y = self.find(self.data,'9')
+        self.generate_possible_children()    
+        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
+        children = []
+        for i in val_list:
+            child = self.shuffle(self.data,x,y,i[0],i[1])
+            if child is not None:
+                child_node = Node(child,self.level+1,0)
+                children.append(child_node)
+        return children
+
+    def generate_possible_children(self):
+        #list_children = self.find_children()
+        
+        children = []
+        values_to_move = []
+        goal = [['1', '2', '3'], ['4', '5', '6'], [ '7', '8','9']]
+        puz = self.data
+        for i in range(3):
+            for j in range(3):
+                if(puz[i][j] != goal[i][j]):
+                    
+                    #print("puz ", puz)
+                    values_to_move.append(puz[i][j])
+                    self.possible_moves(children, puz, i, j)
+        
+        #for i in range(len(children)):
+        #  print("children ", children[i].data)
+        #x = input()
+        
+
+    def possible_moves(self, children, puz, col, row):
+        self.moveRight(children, puz, col, row)
+        #self.moveLeft(children, col, row)
+        #self.moveUp(children, col, row)
+        #self.moveDown(children, col, row)
+        
+
+        
+
+    def moveRight( self, children, puz, col, row):
+        if (row <= 1):
+            temp = puz[col][row]
+            puz[col][row] = puz[col][row + 1]
+            puz[col][row + 1] = temp 
+            #print("Right ",puz)
+            child_node = Node(puz, self.level + 1, 0)
+            children.append(child_node)
+        
+
+    def moveLeft( self, children, col, row):
+        puz = self.data
+        if (row > 0):
+            temp = puz[col][row]
+            puz[col][row] = puz[col][row - 1]
+            puz[col][row - 1] = temp 
+            print("Left ",puz)
+        child_node = Node(puz, self.level+1, 0)
+        children.append(child_node)
+        
+
+    def moveUp( self, children, col, row):
+        puz = self.data
+        if (col > 0):
+            temp = puz[col][row]
+            puz[col][row] = puz[col - 1][row]
+            puz[col - 1][row] = temp 
+            print("Up ",puz)
+        child_node = Node(puz, self.level+1, 0)
+        children.append(child_node)
+        
+
+    def moveDown( self, children, col, row):
+        puz = self.data
+        if (col <= 1):
+            temp = puz[col][row]
+            puz[col][row] = puz[col + 1][row]
+            puz[col + 1][row] = temp 
+            print("Down ",puz)
+        child_node = Node(puz, self.level+1, 0)
+        children.append(child_node)
+        
+
+    def shuffle(self,puz,x1,y1,x2,y2):
+        """ Move the blank space in the given direction and if the position value are out
+            of limits the return None """
+        if x2 >= 0 and x2 < 3 and y2 >= 0 and y2 < 3:
+            temp_puz = []
+            temp_puz = self.copy(puz)
+            temp = temp_puz[x2][y2]
+            temp_puz[x2][y2] = temp_puz[x1][y1]
+            temp_puz[x1][y1] = temp
+            return temp_puz
+        else:
+            return None
+            
+
+    def copy(self,root):
+        """ Copy function to create a similar matrix of the given node"""
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp    
+            
+    def find(self,puz,x):
+        for i in range(0,len(self.data)):
+            for j in range(0,len(self.data)):
+                if puz[i][j] == x:
+                    return i,j
+                   
+
+class Puzzle:
+    def __init__(self,size):
+        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
+        self.n = size
+        self.open = []
+        self.closed = []
+
+
+
+    def f(self, start, goal):
+        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
+        
+        returnVal = self.h(start.data, goal)
+        #print("level ", start.level,"returnVal ", returnVal )
+        returnVal += start.level
+        return returnVal
+
+    def h(self,start,goal):
+        """ Calculates the different between the given puzzles """
+        temp = 0
+        for i in range(0,self.n):
+            for j in range(0,self.n):
+                #print(i,j," ",start[i][j], " ",goal[i][j])
+                if start[i][j] != goal[i][j]:
+                    temp += 1
+        #print("temp ", temp)
+        return temp
+        
+
+    def process(self):
+        start = [['1', '2', '3'], ['4', '5', '6'], [ '9', '8', '7']]
+        goal = [['1', '2', '3'], ['4', '5', '6'], [ '7', '8','9']]
+        start = Node(start,0,0)
+        start.fval = self.f(start,goal)
+        """ Put the start node in the open list"""
+        self.open.append(start)
+        while True:
+            cur = self.open[0]
+            print("")
+            for i in cur.data:
+                for j in i:
+                    print(j,end=" ")
+                print("")
+            """ If the difference between current and goal node is 0 we have reached the goal node"""
+            if(self.h(cur.data,goal) == 0):
+                break
+            for i in cur.generate_child():
+                i.fval = self.f(i,goal)
+                self.open.append(i)
+            self.closed.append(cur)
+            self.open.remove(self.open[0])
+
+            """ sort the open list based on f value """
+            self.open.sort(key = lambda x:x.fval)
+            temp = self.open[0]
+            self.open.clear()
+            self.open.append(temp)
+            #print("len(self.open) ",len(self.open)  )
+            #for i in range(len(self.open)):
+             #   print("self.open[",i,"].fval ",self.open[i].fval)
+              #  print(self.open[i].data)
+            #x = input()
+
+
+puz = Puzzle(3)
+puz.process()
